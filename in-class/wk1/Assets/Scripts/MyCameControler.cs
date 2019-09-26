028836d55fb1b890147fd32cf492c950dc9adce5
Hellfire <holdenelling@holdens-MacBook-Pro.local>
2019-09-25 23:08:58 -0400

    thus it is and isn't

---
+++ in-class/wk1/Assets/Scripts/MyCameControler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCameControler : MonoBehaviour
+{
+    public gameObject Logs; 
+    void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Axe")
+		{
+			Logs.SetActive(true);
+            GameObject.SetActive(false);
+		}
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
