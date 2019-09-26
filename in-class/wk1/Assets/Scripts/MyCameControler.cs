bdfc8dde74e697f5f2a7807b7335a6ce9814cc57
Hellfire <holdenelling@holdens-MacBook-Pro.local>
2019-09-26 11:50:12 -0400

    finished

--- in-class/wk1/Assets/Scripts/MyCameControler.cs
+++ in-class/wk1/Assets/Scripts/MyCameControler.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class MyCameControler : MonoBehaviour
 {
-    public gameObject Logs; 
-    void OnTriggerEnter (Collider other)
-	{
-		if (other.tag == "Axe")
-		{
-			Logs.SetActive(true);
-            GameObject.SetActive(false);
-		}
-    }
+   public bool axeRestored = false;
+   public bool goodRed = false;
+   public bool goodBlack = false;
+   public bool badRed = false;
+   public bool badBlack = false;
+   public bool chickenRestored = false;
+
+   public GameObject finishText;
+
+   public void checkForVictory(){
+       if(axeRestored && goodBlack && goodRed && badBlack && badRed && chickenRestored) {
+        finishText.SetActive(true);
+       }
+   }
+
+   public void axeWasRest(){
+       this.axeRestored = true;
+       checkForVictory();
+   }
+   public void goodredClip(){
+       this.goodRed = true;
+       checkForVictory();
+   }
+   public void goodBlackClip(){
+       this.goodBlack = true;
+       checkForVictory();
+   }
+   public void badRedClip(){
+       this.badRed = true;
+       checkForVictory();
+   }
+   public void badBlackClip(){
+       this.badBlack = true;
+       checkForVictory();
+   }
+   public void chickenWasRest(){
+       this.chickenRestored = true;
+       checkForVictory();
+   }
+
 
     // Start is called before the first frame update
     void Start()
